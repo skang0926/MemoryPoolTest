76737ae767ddba719393d03508fb32202ee4ac68
skang0926 <skang0926@naver.com>
2022-04-20 23:19:22 +0900

    # 메모리풀 리팩토링 할당/해제 테스트

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -28,6 +28,38 @@ int main(void)
 	TestHeap(FMemoryBlock64(), num);
 	TestMemoryPool(FMemoryBlock64(), num);
 
+	MemoryPool<FMemoryBlock64>* memoryPool = new MemoryPool<FMemoryBlock64>(5);
+
+	vector<TestClass*> v1;
+	for (int i = 0; i < 5; ++i)
+	{
+		auto test = reinterpret_cast<TestClass*>(memoryPool->Alloc());
+		test->x = i;
+		test->y = -i;
+		v1.push_back(test);
+	}
+
+	for (const auto& v : v1)
+	{
+		v->Print();
+		memoryPool->Free(v);
+	}
+
+	v1.clear();
+
+	for (int i = 0; i < 5; ++i)
+	{
+		auto test = reinterpret_cast<TestClass*>(memoryPool->Alloc());
+		v1.push_back(test);
+	}
+
+	for (const auto& v : v1)
+	{
+		v->Print();
+		memoryPool->Free(v);
+	}
+
+
 	system("pause");
 	return 0;
 }
